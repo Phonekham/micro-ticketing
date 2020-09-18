616a79b2a669a9fdecbabea5e9faee7c4146f877
Phonekham <30440409+Phonekham@users.noreply.github.com>
2020-09-18 23:18:16 +0700

    fix build error in dockerfile

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,10 +4,10 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
-app.get("api/users/currentuser", (req, res) => {
+app.get("/api/users/currentuser", (req, res) => {
   res.send("hi there");
 });
 
 app.listen(3000, () => {
-  console.log("listening to port 3000");
+  console.log("listening to port 3000!!!");
 });
